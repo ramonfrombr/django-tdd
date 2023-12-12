4a1c8b7d03ab4fdc1caf1f0b5e5332f9f78e0e2c
ramonfrombr <ramonfrombr>
2023-12-12 15:44:26 -0300

    Redireciona após POST, e exibe todos items no template

--- listas/tests.py
+++ listas/tests.py
@@ -10,10 +10,23 @@ class TestePaginaInicial(TestCase):
         resposta = self.client.get('/')
         self.assertTemplateUsed(resposta, 'inicio.html')
     
+    def test_salva_itens_apenas_quando_necessario(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_pode_salvar_um_pedido_POST(self):
-        resposta = self.client.post('/', data={'tarefa_texto': "Um novo item"})
-        self.assertIn("Um novo item", resposta.content.decode())
-        self.assertTemplateUsed(resposta, 'inicio.html')
+
+        NOVO_ITEM_TEXTO = "Um novo item"
+        resposta = self.client.post('/', data={'tarefa_texto': NOVO_ITEM_TEXTO})
+
+        self.assertEqual(Item.objects.count(), 1)
+        novo_item = Item.objects.first()
+        self.assertEqual(novo_item.texto, NOVO_ITEM_TEXTO)
+
+    def test_redireciona_apos_POST(self):
+        resposta = self.client.post('/', data={'tarefa_texto': "Novo item"})
+        self.assertEqual(resposta.status_code, 302)
+        self.assertEqual(resposta['location'], '/')
 
     def test_salvar_e_selecionar_itens(self):
         primeiro_item = Item()
@@ -31,3 +44,12 @@ class TestePaginaInicial(TestCase):
         segundo_item_salvo = itens_salvos[1]
         self.assertEqual(primeiro_item_salvo.texto, "Primeiro item")
         self.assertEqual(segundo_item_salvo.texto, "Segundo item")
+
+    def test_exibe_todos_itens_lista(self):
+        Item.objects.create(texto="Item 1")
+        Item.objects.create(texto="Item 2")
+        
+        resposta = self.client.get('/')
+
+        self.assertIn('Item 1', resposta.content.decode())
+        self.assertIn('Item 2', resposta.content.decode())
--- listas/views.py
+++ listas/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import Item
 
 # Create your views here.
 def pagina_inicial(pedido):
+
+    if pedido.method == "POST":
+        Item.objects.create(texto=pedido.POST['tarefa_texto'])
+        return redirect('/')
+    
+    itens = Item.objects.all()
+
     return render(pedido, 'inicio.html', {
-        'nova_tarefa_texto': pedido.POST.get('tarefa_texto', '')
+        'items': itens
     })
