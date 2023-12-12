d9d6b375d29e65c1d6d8e7ebf3c2d457e77c7d27
ramonfrombr <ramonfrombr>
2023-12-12 15:01:39 -0300

    Modelo para Item do app listas e migrations associadas

--- listas/tests.py
+++ listas/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from listas.views import pagina_inicial
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from listas.models import Item
 
 class TestePaginaInicial(TestCase):
     def test_usa_template_inicio(self):
@@ -13,3 +14,20 @@ class TestePaginaInicial(TestCase):
         resposta = self.client.post('/', data={'tarefa_texto': "Um novo item"})
         self.assertIn("Um novo item", resposta.content.decode())
         self.assertTemplateUsed(resposta, 'inicio.html')
+
+    def test_salvar_e_selecionar_itens(self):
+        primeiro_item = Item()
+        primeiro_item.texto = "Primeiro item"
+        primeiro_item.save()
+
+        segundo_item = Item()
+        segundo_item.texto = "Segundo item"
+        segundo_item.save()
+
+        itens_salvos = Item.objects.all()
+        self.assertEqual(itens_salvos.count(), 2)
+
+        primeiro_item_salvo = itens_salvos[0]
+        segundo_item_salvo = itens_salvos[1]
+        self.assertEqual(primeiro_item_salvo.texto, "Primeiro item")
+        self.assertEqual(segundo_item_salvo.texto, "Segundo item")
